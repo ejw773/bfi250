e89b9e901ba9212ae4e4300a8b6e92635dd8767e
ejw773 <ejw773@gmail.com>
2020-11-29 20:16:00 -0500

    adds movie data

--- src/Footer.js
+++ src/Footer.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import './Footer.css'
 
-const Footer = (props) => {
-    return (
-        <div>
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchInput: 'search box test'
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            searchInput: event.target.value
+        })
+    }
+
+    handleSubmit() {
+        console.log(this.state.searchInput)
+    }
+
+    render() {
+        return (
+            <div>
             <nav className="navbar fixed-bottom navbar-dark bg-dark">
-                <a className="navbar-brand" href="http://explore.bfi.org.uk/sightandsoundpolls/2012/" target="_blank">Sight & Sound "Greatest Films of All Time" | 2012 Edition</a>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <a className="navbar-brand" href="https://fanwithamovieyammer.wordpress.com/the-sight-sound-top-250-list/" target="_blank">Sight & Sound "Greatest Films of All Time" | 2012 Edition</a>
+                <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit.bind(this)}>
+                    <input value={this.state.input} onChange={this.handleChange.bind(this)} className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </nav>
         </div>
-    )
+
+
+
+        )
+    }
 }
+
 
 export default Footer;
