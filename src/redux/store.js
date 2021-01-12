5b08bec58c9ac5425682e96f14e4034499f38fc7
ejw773 <ejw773@gmail.com>
2021-01-11 22:34:13 -0500

    removes local storage feature to make way for thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
-function saveToLocalStorage(state) {
-    try {
-        const serialisedState = JSON.stringify(state);
-        localStorage.setItem("persistantState", serialisedState);
-    } catch (event) {
-        console.log(event);
-    }
-}
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
-function loadFromLocalStorage() {
-    try {
-        const serialisedState = localStorage.getItem("persistantState");
-        if (serialisedState === null) return undefined;
-        return JSON.parse(serialisedState);
-    } catch (event) {
-        console.log(event);
-        return undefined;
-    }
-}
-
-export const store = createStore(rootReducer, loadFromLocalStorage(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-store.subscribe(() => saveToLocalStorage(store.getState()));
-
-window.store = store;
+export const store = createStore(rootReducer, composedEnhancer);
+export default store;
