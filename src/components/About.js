e2962d39faa453641db7afc22f3eb6b1ef0697db
Elijah Wilcott <ejw773@gmail.com>
2021-01-12 11:52:11 -0500

    adds menu bar to 'about' page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ProgressBar.css';
 
 const About = () => {
     return (
-        <div id="about-page">
-            <h1>This Will Be The 'About' Page</h1>
-            <Link to="/">Back to Progress Bar</Link> <br />
-            <Link to="/landing">Logout</Link>
+        <div>
+            <Link to="/">
+                <div className="progress progress-all">
+                    <div className="progress-all progress-bar bg-primary" id="view-all" role="progressbar" style={{width:'100%'}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">Back to Progress Bar</div>
+                </div>
+            </Link>
+            <div className="progress progress-all">
+                    <div className="progress-all progress-bar bg-secondary" id="view-all" role="progressbar" style={{width: '100%', textDecoration: 'underline', border: 'solid white 4px'}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">About</div>
+            </div>
+            <div id="about-page">
+                <h1>This Will Be The 'About' Page</h1>
+                <Link to="/landing">Logout</Link>
+            </div>
         </div>
     )
 }
