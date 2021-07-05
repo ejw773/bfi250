0dbc6786572decc8c102c1b855c405598e0434c9
Elijah Wilcott <ejw773@gmail.com>
2021-07-05 11:40:42 -0400

    adds menu bar to login page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,67 @@
 import React from 'react';
+import { Modal, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import './Landing.css'
+import MenuBar from './MenuBar';
+
+class LoginManager extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    };
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+    handleLogin(event) {
+        alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
+        this.toggleModal();
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <MenuBar></MenuBar>
+                <Landing />
+            </div>
+        )
+    }
+}
 
 const Landing = () => {
     return (
         <div id='landing-page'>
-            <h1>This Will Be The Landing Page</h1>
             <Link to="/">Take Me to the Progress Bar</Link>
+            <Modal.Dialog>
+                <Modal.Header closebutton>
+                    <Modal.Title>Login</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Login Form Goes Here</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary">Close</Button>
+                    <Button variant="primary">Login</Button>
+                </Modal.Footer>
+            </Modal.Dialog>
         </div>
     )
 }
 
-export default Landing;
+export default LoginManager;
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import './ProgressBar.css'
 import { connect } from 'react-redux';
+import { ProgressBar } from 'react-bootstrap'
 
+const MenuBar = () => {
+    return (
+        <div>
+            <ProgressBar now={100} label="Welcome to the BFI 250 Progress Bar" className="progress-all"/>
+            <ProgressBar className="progress progress-sections">
+                <ProgressBar variant="danger" now={33} label="Login" className="progress-bar" />
+                <ProgressBar variant="warning" now={33} label="About" className="progress-bar" />
+                <ProgressBar variant="success" now={34} label="Something" className="progress-bar" /> 
+            </ProgressBar>
+        </div>
+    )
+}
+
+export default MenuBar;
